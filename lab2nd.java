73cd6285911f025e17f360de18c1f1d730b1f423
NILISHATAWADE54 <130459273+NILISHATAWADE54@users.noreply.github.com>
2024-10-14 00:13:27 +0530

    Add files via upload

---
+++ lab2nd.java
@@ -0,0 +1,13 @@
+package FirstClass;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		System.out.println("helloworld");
+		System.out.println("my number is:"+num);
+
+	}
+
+}
---
+++ lab3rd.java
@@ -0,0 +1,24 @@
+package datatype;
+
+public class PrimitiveDT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag=true;
+		System.out.println(flag);
+		 byte num1=14;
+		 System.out.println(num1);
+		 long num2=123455678;
+		 System.out.println(num2);
+		 int number=35;
+		 System.out.println(number);
+		 short num3=12;
+		 System.out.println(num3);
+		 float num4=1.75f;
+		 System.out.println(num4);
+		 char myGrade='A';
+		 System.out.println(myGrade);
+
+	}
+
+}
