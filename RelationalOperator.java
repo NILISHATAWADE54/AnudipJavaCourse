3358ead68ee3bb08cf352ce90811f662c480c5d0
NILISHATAWADE54 <130459273+NILISHATAWADE54@users.noreply.github.com>
2024-10-15 13:58:37 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		int x=10;
+	    int y=20;
+	    boolean rel=x<=y;
+	    System.out.println(rel);
+        boolean res=x==y;
+        System.out.println(res);
+        boolean r=x!=y;
+        System.out.println(r);
+        boolean n=x>=y;
+        System.out.println(n);
+	}
+
+}
