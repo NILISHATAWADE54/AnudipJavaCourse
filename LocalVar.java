e74900a08b2190adddc5fe0d4159d0551b4b6912
NILISHATAWADE54 <130459273+NILISHATAWADE54@users.noreply.github.com>
2024-10-17 12:00:28 +0530

    Add files via upload

---
+++ LocalVar.java
@@ -0,0 +1,91 @@
+package varibledemo;
+class Employee{
+	int id;
+	String name;
+	int age;
+	double salary;
+}
+
+public class LocalVar {
+
+	public static void main(String[] args) {
+		// 1st employee
+		Employee emp1= new Employee();
+		emp1.id=1;
+		emp1.name="mihir";
+		emp1.age=21;
+		emp1.salary=10000;
+		System.out.println("id:"+emp1.id+" "+"name:"+emp1.name+" "+"age:"+emp1.age+"salary:"+emp1.salary);
+		
+		Employee emp2= new Employee();
+		emp2.id=2;
+		emp2.name="raj";
+		emp2.age=22;
+		emp2.salary=20000;
+		System.out.println("id:"+emp2.id+" "+"name:"+emp2.name+" "+"age:"+emp2.age+"salary:"+emp2.salary);
+		
+		Employee emp3= new Employee();
+		emp3.id=3;
+		emp3.name="abc";
+		emp3.age=23;
+		emp3.salary=30000;
+		System.out.println("id:"+emp3.id+" "+"name:"+emp3.name+" "+"age:"+emp3.age+"salary:"+emp3.salary);
+		
+
+		Employee emp4= new Employee();
+		emp4.id=4;
+		emp4.name="abc";
+		emp4.age=24;
+		emp4.salary=40000;
+		System.out.println("id:"+emp4.id+" "+"name:"+emp4.name+" "+"age:"+emp4.age+"salary:"+emp4.salary);
+
+		Employee emp5= new Employee();
+		emp5.id=5;
+		emp5.name="abc";
+		emp5.age=29;
+		emp5.salary=49000;
+		System.out.println("id:"+emp5.id+" "+"name:"+emp5.name+" "+"age:"+emp5.age+"salary:"+emp5.salary);
+
+		Employee emp6= new Employee();
+		emp6.id=6;
+		emp6.name="mahi";
+		emp6.age=22;
+		emp6.salary=50000;
+		System.out.println("id:"+emp6.id+" "+"name:"+emp6.name+" "+"age:"+emp6.age+"salary:"+emp6.salary);
+
+		Employee emp7= new Employee();
+		emp7.id=7;
+		emp7.name="dgf";
+		emp7.age=28;
+		emp7.salary=99000;
+		System.out.println("id:"+emp7.id+" "+"name:"+emp7.name+" "+"age:"+emp7.age+"salary:"+emp7.salary);
+
+		Employee emp8= new Employee();
+		emp8.id=8;
+		emp8.name="hhh";
+		emp8.age=27;
+		emp8.salary=90000;
+		System.out.println("id:"+emp8.id+" "+"name:"+emp8.name+" "+"age:"+emp8.age+"salary:"+emp8.salary);
+
+		Employee emp9= new Employee();
+		emp9.id=9;
+		emp9.name="hey";
+		emp9.age=26;
+		emp9.salary=80000;
+		System.out.println("id:"+emp9.id+" "+"name:"+emp9.name+" "+"age:"+emp9.age+"salary:"+emp9.salary);
+		
+		Employee emp10= new Employee();
+		emp10.id=10;
+		emp10.name="rrr";
+		emp10.age=32;
+		emp10.salary=90000;
+		System.out.println("id:"+emp10.id+" "+"name:"+emp10.name+" "+"age:"+emp10.age+"salary:"+emp10.salary);
+
+
+
+
+
+
+	}
+
+}
